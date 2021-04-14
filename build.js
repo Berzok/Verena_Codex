378a76d729b9441d77682195326fc7b5b3eca640
Berzok <34751752+Berzok@users.noreply.github.com>
2021-04-14 15:42:24 +0200

    Initial commit

---
+++ build.js
@@ -0,0 +1,15 @@
+const { exec } = require("child_process");
+
+function exec_handler(error, stdout, stderr){
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+}
+
+exec("cd sigmar_codex && neu build --release", exec_handler);
